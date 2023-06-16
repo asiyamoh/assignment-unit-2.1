745989ab3d0ae5a7e68da4ace4b87211aa433034
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-06-16 13:24:08 -0500

    made changes to aboutme.js file

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -2,10 +2,10 @@
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Asiya';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-let lastName = ' Mohamed';
+let lastName = 'Mohamed';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-let fullName = firstName + lastName;
+let fullName = firstName + " " +  lastName;
 // 4 - Console log the value of `fullName`
 console.log(fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
@@ -13,7 +13,7 @@ let luckyNumber = 1;
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
-console.log('My name is '+ fullName + ' and I think ' +  luckyNumber + ' is a winner!');
+console.log('My name is '+ fullName + ', ' + 'and I think ' +  luckyNumber + ' is a winner!');
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
